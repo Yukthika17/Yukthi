979d17478f4c2fea90eeae9f1864b190c4a0f2e0
YUKTHIKA.A <102223836+Yukthika17@users.noreply.github.com>
2022-03-27 07:04:57 -0800

    Add files via upload

---
+++ Reverse prgrm.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df = pd.read_excel("D:/IMARTICUS WORK/pizza.xlsx")
+
