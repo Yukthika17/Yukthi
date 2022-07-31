e27dbeb4fabc13b89439b8b6af3c09ceb1b30420
YUKTHIKA.A <102223836+Yukthika17@users.noreply.github.com>
2022-07-31 11:28:54 +0530

    Add files via upload

--- SQL.sql
+++ SQL.sql
@@ -13,3 +13,10 @@ SELECT ord_no, purch_amt, ord_date, salesman_id
 FROM orders 
 WHERE salesman_id IN(SELECT salesman_id FROM salesman WHERE commision = (SELECT MAX(commision) FROM salesman));
 
+SELECT customer.cust_name AS "Customer",
+customer.grade AS "Grade",orders.ord_no AS "Order No."
+FROM orders, salesman, customer
+WHERE orders.customer_id = customer.customer_id
+AND orders.salesman_id = salesman.salesman_id
+AND salesman.city IS NOT NULL
+AND customer.grade IS NOT NULL;
